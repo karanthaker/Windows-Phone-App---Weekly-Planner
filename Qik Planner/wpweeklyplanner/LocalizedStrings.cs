af7b43a6ac9bf20906f0cdf7050049e04d9275dc
Karan Thaker <karan.thaker@outlook.com>
2013-02-11 00:09:58 -0500

    initial commit

---
+++ Qik Planner/wpweeklyplanner/LocalizedStrings.cs
@@ -0,0 +1,31 @@
+﻿/*
+Karan Thaker
+karan.thaker@hotmail.com
+ */
+
+namespace wpweeklyplanner
+{
+    /// <summary>
+    /// Helper class for accessing application resources from XAML, especially for localization
+    /// </summary>
+    public class LocalizedStrings
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LocalizedStrings()
+        {
+        }
+
+        /// <summary>
+        /// Class variable for instance of AppResources
+        /// </summary>
+        private static wpweeklyplanner.AppResources localizedResources = new wpweeklyplanner.AppResources();
+
+        /// <summary>
+        /// Property for accessing AppResources
+        /// </summary>
+        public wpweeklyplanner.AppResources LocalizedResources { get { return localizedResources; } }
+
+    }
+}
